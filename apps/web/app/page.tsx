83f74fa471bd748c42a76dc7f71fad6b596030d3
Mac-11 <mac-11@Mac-11s-Mac-mini.local>
2026-02-11 12:01:52 +0800

    fixed position, changed background

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.sidebar}></div>
-      <div className={styles.viewport}></div>
+      <div className={styles.viewport}>
+        <div className={styles.header}></div>
+        <div></div>
+      </div>
     </div>
   );
 }
