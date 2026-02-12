6160eaadb1712f04d4538907c6438d7fa4db4e25
Mac-11 <mac-11@Mac-11s-Mac-mini.local>
2026-02-12 10:25:53 +0800

    minor changes

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,24 @@
 import styles from "./page.module.css";
+import { Logo } from "./_icons/logo.js";
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <div className={styles.sidebar}></div>
-      <div className={styles.viewport}>
+      <div className={styles.sidebar}>
+        <div className={styles.topSection}>
+          <div className={styles.logoPlacement}>
+            <div className={styles.logoText}>
+              <div className={styles.logoContainer}>
+                <Logo />
+              </div>
+              <p className={styles.name}> Олимпиад </p>{" "}
+            </div>
+
+            <div className={styles.title}> Багш </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightViewport}>
         <div className={styles.header}></div>
         <div></div>
       </div>
